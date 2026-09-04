bc9a8690296879ba9dac81cb207883df4e8547ea
ShineYamamoto <love.of.240.yen@gmail.com>
2026-09-05 00:25:39 +0900

    初回コミット

---
+++ LessonManagerApp/src/main/java/com/ShineYamamoto/LessonManagerApp/LessonManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.ShineYamamoto.LessonManagerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonManagerAppApplication.class, args);
+	}
+
+}
---
+++ LessonManagerApp/src/test/java/com/ShineYamamoto/LessonManagerApp/LessonManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShineYamamoto.LessonManagerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
