4c0cfb30441ca87ca572e46f03eacd7a1b08ce76
ShineYamamoto <love.of.240.yen@gmail.com>
2026-09-15 15:08:08 +0900

    layout-dialectを導入、ユーザー一覧画面に反映

--- LessonManagerApp/src/main/java/com/ShineYamamoto/LessonManagerApp/login/controller/LoginController.java
+++ LessonManagerApp/src/main/java/com/ShineYamamoto/LessonManagerApp/login/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ShineYamamoto.LessonManagerApp.common.service.CountryCodeService;
 
@@ -26,5 +27,11 @@ public class LoginController {
 		model.addAttribute("countryList", countryCodeService.getCountryList(locale));
 		
 		return "login/login";
+	}
+	
+	/** ユーザー一覧画面にリダイレクト */
+	@PostMapping("/login")
+	public String postLogin() {
+		return "redirect:/admin/user/list";
 	}
 }
