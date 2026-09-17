8265211e9103d205cf8368830f69200132a3e614
ShineYamamoto <love.of.240.yen@gmail.com>
2026-09-18 03:00:56 +0900

    goalLevelIdのバリデーションチェックを修正

--- LessonManagerApp/src/main/java/com/ShineYamamoto/LessonManagerApp/user/validation/GoalCheckValidator.java
+++ LessonManagerApp/src/main/java/com/ShineYamamoto/LessonManagerApp/user/validation/GoalCheckValidator.java
@@ -3,29 +3,30 @@ package com.ShineYamamoto.LessonManagerApp.user.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import com.ShineYamamoto.LessonManagerApp.user.domain.service.UserService;
+import com.ShineYamamoto.LessonManagerApp.goallevel.domain.service.GoalLevelService;
 
 
 public class GoalCheckValidator implements ConstraintValidator<GoalCheck, Integer> {
 	
-	private final UserService userService;
+	// private final UserService userService;
+	private final GoalLevelService goalLevelService;
 	
-	public GoalCheckValidator(UserService userService) {
-		this.userService = userService;
+	public GoalCheckValidator(GoalLevelService goalLevelService) {
+		this.goalLevelService = goalLevelService;
 	}
 	
 	// 初期化処理
 	@Override
 	public boolean isValid(
-			Integer goal,
+			Integer goalLevelId,
 			ConstraintValidatorContext context) {
 		
-		// nullのチェックは@Nullでチェックする
-		if (goal == null) {
+		// nullは@NotNullでチェックする
+		if (goalLevelId == null) {
 			return true;
 		}
 		
 		// 選択された値が目標レベル一覧（UserService.getGoalMap()に含まれているか
-		return userService.getGoalMap().containsValue(goal);
+		return goalLevelService.isSelectable(goalLevelId);
 	}
 }
