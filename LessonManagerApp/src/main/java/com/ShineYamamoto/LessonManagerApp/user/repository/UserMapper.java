2dffc33538d9fe79463bf5880890a71546c08f84
ShineYamamoto <love.of.240.yen@gmail.com>
2026-09-24 15:42:18 +0900

    UserMapper.java引数に@Paramを付ける

--- LessonManagerApp/src/main/java/com/ShineYamamoto/LessonManagerApp/user/repository/UserMapper.java
+++ LessonManagerApp/src/main/java/com/ShineYamamoto/LessonManagerApp/user/repository/UserMapper.java
@@ -3,6 +3,7 @@ package com.ShineYamamoto.LessonManagerApp.user.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Pageable;
 
 import com.ShineYamamoto.LessonManagerApp.user.domain.model.User;
@@ -17,13 +18,18 @@ public interface UserMapper {
 	public int count(User user);
 	
 	/** ユーザー取得（複数） */
-	public List<User> findMany(User user, Pageable pageable);
+	public List<User> findMany(
+			@Param("user") User user,
+			@Param("pageable") Pageable pageable);
 	
 	/** ユーザーIDから1件取得 */
 	public User findById(Long userId);
 	
 	/** ユーザー更新(1件) */
-	public int updateById(Long userId, String password, String userName);
+	public int updateById(
+			@Param("userId") Long userId,
+			@Param("password") String password,
+			@Param("userName")  String userName);
 	
 	/** ユーザー削除（1件） */
 	public int deleteById(Long userId);
